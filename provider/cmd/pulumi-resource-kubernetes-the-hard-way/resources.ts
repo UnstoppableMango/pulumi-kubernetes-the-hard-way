41cea73577df2acc7b4c33c2fcddcd423e6f1b62
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-26 21:43:49 -0500

    Refactor resources to be more DRY

---
+++ provider/cmd/pulumi-resource-kubernetes-the-hard-way/resources.ts
@@ -0,0 +1,36 @@
+import { ComponentResource, ComponentResourceOptions, Inputs } from '@pulumi/pulumi';
+import { Certificate } from './certificate';
+import { ClusterPki } from './clusterPki';
+import { RemoteFile } from './remoteFile';
+import { RootCa } from './rootCa';
+
+export type ConstructComponent<T extends ComponentResource = ComponentResource>
+  = (name: string, inputs: any, options: ComponentResourceOptions) => T;
+
+export type ResourceConstructor = {
+  readonly 'kubernetes-the-hard-way:index:Certificate': ConstructComponent<Certificate>;
+  readonly 'kubernetes-the-hard-way:index:ClusterPki': ConstructComponent<ClusterPki>;
+  readonly 'kubernetes-the-hard-way:index:RemoteFile': ConstructComponent<RemoteFile>;
+  readonly 'kubernetes-the-hard-way:index:RootCa': ConstructComponent<RootCa>;
+};
+
+const resources: ResourceConstructor = {
+  "kubernetes-the-hard-way:index:Certificate": (...args) => new Certificate(...args),
+  "kubernetes-the-hard-way:index:ClusterPki": (...args) => new ClusterPki(...args),
+  "kubernetes-the-hard-way:index:RemoteFile": (...args) => new RemoteFile(...args),
+  "kubernetes-the-hard-way:index:RootCa": (...args) => new RootCa(...args),
+};
+
+export function construct(
+  name: string,
+  type: string,
+  inputs: Inputs,
+  options: ComponentResourceOptions,
+): ComponentResource | undefined {
+  const genericResources: Record<string, ConstructComponent> = resources;
+  const resource = genericResources[type];
+  if (resource === undefined) {
+    return undefined;
+  }
+  return resource(name, inputs, options);
+}
--- provider/cmd/pulumi-resource-kubernetes-the-hard-way/util.ts
+++ provider/cmd/pulumi-resource-kubernetes-the-hard-way/util.ts
@@ -1,4 +1,6 @@
+import { ComponentResource } from '@pulumi/pulumi';
 import { AllowedUsage } from './types';
+import { ConstructResult } from '@pulumi/pulumi/provider';
 
 export function isAllowedUsage(value: string): value is AllowedUsage {
   return Object.values(AllowedUsage).some(x => x === value);
@@ -14,4 +16,27 @@ export function toAllowedUsage(value: string | string[]): AllowedUsage | Allowed
   } else {
     throw new Error(`unsupported allowedUsage: ${value}`);
   }
+}
+
+/**
+ * https://github.com/pulumi/pulumi-awsx/blob/0390f0dd5b166861ef240e21fe676959fed041d5/awsx/utils.ts#L55
+ * @internal
+ * */
+export function resourceToConstructResult<T extends ComponentResource>(
+  resource: T,
+): ConstructResult {
+  const state = Object.fromEntries(
+    Object.entries(resource).filter(
+      ([key, value]) =>
+        key !== "urn" &&
+        !key.startsWith("get") &&
+        !key.startsWith("_") &&
+        typeof value !== "function" &&
+        value !== undefined,
+    ),
+  );
+  return {
+    urn: resource.urn,
+    state,
+  };
 }
