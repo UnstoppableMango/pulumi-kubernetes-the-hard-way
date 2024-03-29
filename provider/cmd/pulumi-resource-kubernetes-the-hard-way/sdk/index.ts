d6bfc30f9bc984adfab700e5d939f8ffba256771
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-29 16:23:14 -0500

    Add sdk args exports

---
+++ provider/cmd/pulumi-resource-kubernetes-the-hard-way/sdk/index.ts
@@ -0,0 +1,8 @@
+export { MkdirArgs, TarArgs, WgetArgs } from '../../../../sdk/nodejs/tools';
+export {
+  CertificateArgs,
+  ClusterPkiArgs,
+  EncryptionKeyArgs,
+  RemoteFileArgs,
+  RootCaArgs,
+} from '../../../../sdk/nodejs';
