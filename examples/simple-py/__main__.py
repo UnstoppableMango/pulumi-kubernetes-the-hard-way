7a22e32615dd7b5907e514cbe3b2055c31904e68
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-30 00:04:40 -0500

    Fixing test imports

--- examples/simple-py/__main__.py
+++ examples/simple-py/__main__.py
@@ -2,13 +2,13 @@
 
 import pulumi_kubernetes_the_hard_way as kthw
 
-ca = kthw.RootCa("simple",
-                 algorithm=kthw.Algorithm.RSA,
+ca = kthw.tls.RootCa("simple",
+                 algorithm=kthw.tls.Algorithm.RSA,
                  validity_period_hours=256)
 
-cert = kthw.Certificate("simple",
-                        algorithm=kthw.Algorithm.RSA,
-                        allowed_uses=[kthw.AllowedUsage.CERT_SIGNING],
+cert = kthw.tls.Certificate("simple",
+                        algorithm=kthw.tls.Algorithm.RSA,
+                        allowed_uses=[kthw.tls.AllowedUsage.CERT_SIGNING],
                         ca_cert_pem=ca.cert_pem,
                         ca_private_key_pem=ca.private_key_pem,
                         validity_period_hours=256)
