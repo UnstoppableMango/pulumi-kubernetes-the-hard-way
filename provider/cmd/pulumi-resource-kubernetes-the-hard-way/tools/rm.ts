82b1d037335797dd2172979fa7efc3d7ef46b9d0
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-30 23:15:21 -0500

    Add onDelete option

--- provider/cmd/pulumi-resource-kubernetes-the-hard-way/tools/rm.ts
+++ provider/cmd/pulumi-resource-kubernetes-the-hard-way/tools/rm.ts
@@ -8,6 +8,7 @@ export interface RmArgs {
   dir?: Input<boolean>;
   files: Input<string | Input<string>[]>;
   force?: Input<boolean>;
+  onDelete?: boolean;
   recursive?: Input<boolean>;
   verbose?: Input<boolean>;
 }
@@ -17,6 +18,7 @@ export class Rm extends ComponentResource {
   public readonly dir!: Output<boolean>;
   public readonly files!: Output<string[]>;
   public readonly force!: Output<boolean>;
+  public readonly onDelete!: Output<boolean>;
   public readonly recursive!: Output<boolean>;
   public readonly verbose!: Output<boolean>;
 
@@ -29,6 +31,7 @@ export class Rm extends ComponentResource {
     const dir = output(args.dir ?? false);
     const files = output(args.files).apply(toArray);
     const force = output(args.force ?? false);
+    const onDelete = args.onDelete ?? false;
     const recursive = output(args.recursive ?? false);
     const verbose = output(args.verbose ?? false);
 
@@ -41,13 +44,15 @@ export class Rm extends ComponentResource {
 
     const command = new Command(name, {
       connection: args.connection,
-      create: builder.command,
+      create: !onDelete ? builder.command : undefined,
+      delete: onDelete ? builder.command : undefined,
     }, { parent: this });
 
     this.command = command;
     this.dir = dir;
     this.files = files;
     this.force = force;
+    this.onDelete = output(onDelete);
     this.recursive = recursive;
     this.verbose = verbose;
 
@@ -56,6 +61,7 @@ export class Rm extends ComponentResource {
       dir,
       files,
       force,
+      onDelete,
       recursive,
       verbose,
     });
