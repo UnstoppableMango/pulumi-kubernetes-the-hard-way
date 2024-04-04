52fa5a14afa58e805f01831a23e37b858d91b878
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-04 16:43:16 -0500

    Implementing more resources

--- provider/cmd/pulumi-resource-kubernetes-the-hard-way/remote/index.ts
+++ provider/cmd/pulumi-resource-kubernetes-the-hard-way/remote/index.ts
@@ -2,4 +2,6 @@ export * from './download';
 export * from './etcdInstall';
 export * from './file';
 export * from './kubeApiServerInstall';
+export * from './kubeControllerManagerInstall';
+export * from './kubeSchedulerInstall';
 export * from './systemdService';
---
+++ provider/cmd/pulumi-resource-kubernetes-the-hard-way/remote/kubeControllerManagerInstall.ts
@@ -0,0 +1,62 @@
+import { ComponentResourceOptions, interpolate, output } from '@pulumi/pulumi';
+import * as schema from '../schema-types';
+import { Download } from './download';
+import { Mkdir, Mktemp, Mv, Rm } from '../tools';
+
+export class KubeControllerManagerInstall extends schema.KubeControllerManagerInstall {
+  constructor(name: string, args: schema.KubeControllerManagerInstallArgs, opts?: ComponentResourceOptions) {
+    super(name, args, opts);
+
+    const architecture = output(args.architecture ?? 'amd64');
+    const connection = output(args.connection);
+    const installDirectory = output(args.installDirectory ?? '/usr/local/bin');
+    const version = output(args.version ?? '1.29.2');
+    const url = interpolate`https://storage.googleapis.com/kubernetes-release/release/v${version}/bin/linux/${architecture}/kube-apiserver`;
+
+    const tmp = new Mktemp(name, {
+      connection,
+      directory: true,
+    }, { parent: this });
+
+    const tmpDir = tmp.stdout;
+
+    const download = new Download(name, {
+      connection,
+      destination: tmpDir,
+      url,
+    }, { parent: this, dependsOn: tmp });
+
+    const mkdir = new Mkdir(name, {
+      connection,
+      directory: installDirectory,
+      parents: true,
+    }, { parent: this });
+
+    const binPath = interpolate`${installDirectory}/kube-apiserver`;
+
+    const mv = new Mv(name, {
+      connection,
+      source: interpolate`${download.destination}/kube-apiserver`,
+      dest: binPath,
+    }, { parent: this, dependsOn: [tmp, mkdir, download] });
+
+    const rm = new Rm(name, {
+      connection,
+      files: tmpDir,
+      force: true,
+      recursive: true,
+    }, { parent: this, dependsOn: mv });
+
+    this.architecture = architecture;
+    this.connection = connection;
+    this.installDirectory = installDirectory;
+    this.version = version;
+
+    this.registerOutputs({
+      architecture,
+      connection,
+      installDirectory,
+      version,
+    });
+  }
+}
