a155b2cf32591a339321ed4ccf67a3e5f45f2386
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-07 18:00:59 -0500

    I think the last of the systemd types

--- schemagen/pkg/gen/remote.go
+++ schemagen/pkg/gen/remote.go
@@ -22,7 +22,7 @@ func generateRemote(commandSpec schema.PackageSpec) schema.PackageSpec {
 				{Value: "arm64"},
 			},
 		},
-		remoteMod + "SystemdInstallSeciont": {
+		remoteMod + "SystemdInstallSection": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "https://www.freedesktop.org/software/systemd/man/latest/systemd.unit.html#%5BInstall%5D%20Section%20Options",
 				Type:        "object",
@@ -84,7 +84,54 @@ func generateRemote(commandSpec schema.PackageSpec) schema.PackageSpec {
 						Description: "Commands that are executed when this service is started.",
 						TypeSpec:    typeSpecs.String, // This can technically be an array when type is oneshot
 					},
-					"exitType": {},
+					"exitType": {
+						Description: "Specifies when the manager should consider the service to be finished.",
+						TypeSpec:    schema.TypeSpec{Ref: localType("SystemdServiceExitType", "remote")},
+					},
+					"restart": {
+						Description: "Configures whether the service shall be restarted when the service process exits, is killed, or a timeout is reached.",
+						TypeSpec:    schema.TypeSpec{Ref: localType("SystemdServiceRestart", "remote")},
+					},
+					"restartSec": {
+						Description: "Configures the time to sleep before restarting a service (as configured with Restart=).",
+						TypeSpec:    typeSpecs.String,
+					},
+					"type": {
+						Description: "Configures the mechanism via which the service notifies the manager that the service start-up has finished.",
+						TypeSpec:    schema.TypeSpec{Ref: localType("SystemdServiceType", "remote")},
+					},
+				},
+			},
+		},
+		remoteMod + "SystemdUnitSection": {
+			ObjectTypeSpec: schema.ObjectTypeSpec{
+				Description: "https://www.freedesktop.org/software/systemd/man/latest/systemd.unit.html#",
+				Type:        "object",
+				Properties: map[string]schema.PropertySpec{
+					"bindsTo": {
+						Description: "Configures requirement dependencies, very similar in style to Requires=.",
+						TypeSpec:    typeSpecs.ArrayOfStrings,
+					},
+					"description": {
+						Description: "A short human readable title of the unit.",
+						TypeSpec:    typeSpecs.String,
+					},
+					"documentation": {
+						Description: "A space-separated list of URIs referencing documentation for this unit or its configuration.",
+						TypeSpec:    typeSpecs.ArrayOfStrings,
+					},
+					"requires": {
+						Description: "Similar to Wants=, but declares a stronger requirement dependency.",
+						TypeSpec:    typeSpecs.ArrayOfStrings,
+					},
+					"requisite": {
+						Description: "Similar to Requires=. However, if the units listed here are not started already, they will not be started and the starting of this unit will fail immediately.",
+						TypeSpec:    typeSpecs.ArrayOfStrings,
+					},
+					"wants": {
+						Description: "Configures (weak) requirement dependencies on other units.",
+						TypeSpec:    typeSpecs.ArrayOfStrings,
+					},
 				},
 			},
 		},
