1d106c272462100bc71cc551ff9da59a0f934317
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-07 16:46:44 -0500

    File resource

--- schemagen/pkg/gen/remote.go
+++ schemagen/pkg/gen/remote.go
@@ -35,6 +35,7 @@ func generateRemote(commandSpec schema.PackageSpec) schema.PackageSpec {
 		remoteMod + "CrictlInstall":         generateArchiveInstall(commandSpec, "Installs crictl on a remote system", "crictl"),
 		remoteMod + "Download":              generateDownload(commandSpec),
 		remoteMod + "EtcdInstall":           generateArchiveInstall(commandSpec, "Installs etcd on a remote system", "etcd", "etcdctl"),
+		remoteMod + "File":                  generateFile(commandSpec),
 		remoteMod + "KubeApiServerInstall":  generateBinaryInstall(commandSpec, "Installs kube-apiserver on a remote system."),
 		remoteMod + "KubeControllerManager": generateBinaryInstall(commandSpec, "Installs kube-controller-manager on a remote system."),
 		remoteMod + "KubectlInstall":        generateBinaryInstall(commandSpec, "Installs kubectl on a remote system."),
@@ -290,6 +291,63 @@ func generateDownload(commandSpec schema.PackageSpec) schema.ResourceSpec {
 		IsComponent: true,
 		ObjectTypeSpec: schema.ObjectTypeSpec{
 			Description: "Downloads the file specified by `url` onto a remote system.",
+			Properties:  outputs,
+			Required:    requiredOutputs,
+		},
+		InputProperties: inputs,
+		RequiredInputs:  requiredInputs,
+	}
+}
+
+func generateFile(commandSpec schema.PackageSpec) schema.ResourceSpec {
+	command := commandSpec.Resources["command:remote:Command"]
+
+	inputs := map[string]schema.PropertySpec{
+		"connection": {
+			Description: "The parameters with which to connect to the remote host.",
+			TypeSpec:    schema.TypeSpec{Ref: refType(commandSpec, "Connection", "remote")},
+		},
+		"content": {
+			Description: "The content of the file.",
+			TypeSpec:    typeSpecs.String,
+		},
+		"path": {
+			Description: "The path to the file on the remote host.",
+			TypeSpec:    typeSpecs.String,
+		},
+	}
+
+	requiredInputs := []string{
+		"connection",
+		"content",
+		"path",
+	}
+
+	outputs := map[string]schema.PropertySpec{
+		"command": {
+			Description: "The executed command.",
+			TypeSpec:    schema.TypeSpec{Ref: refResource(commandSpec, "Command", "remote")},
+		},
+		"stderr": command.Properties["stderr"],
+		"stdin":  command.Properties["stdin"],
+		"stdout": command.Properties["stdout"],
+	}
+	maps.Copy(outputs, inputs)
+
+	requiredOutputs := []string{
+		"command",
+		"connection",
+		"content",
+		"path",
+		"stderr",
+		"stdin",
+		"stdout",
+	}
+
+	return schema.ResourceSpec{
+		IsComponent: true,
+		ObjectTypeSpec: schema.ObjectTypeSpec{
+			Description: "",
 			Properties:  outputs,
 			Required:    requiredOutputs,
 		},
