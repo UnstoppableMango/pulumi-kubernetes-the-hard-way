51f2a63ccaa1f06d897b33cbc001dda8f6ff0b5b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-12 21:30:56 -0500

    Updated etcdService.go with map functionality
    The etcdService.go file has been updated to include the "maps" package. The 'restartSec' property type has been changed from string to integer. The 'ip' output property has been removed and a copy function from the maps package is used to copy inputs into outputs. The required inputs have also been updated, removing 'ip'.

--- schemagen/pkg/gen/remote/etcdService.go
+++ schemagen/pkg/gen/remote/etcdService.go
@@ -1,6 +1,8 @@
 package remote
 
 import (
+	"maps"
+
 	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/props"
 	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/types"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
@@ -15,31 +17,26 @@ func generateEtcdService(commandSpec schema.PackageSpec) schema.ResourceSpec {
 			Description: "Optionally override the systemd service restart behaviour. Defaults to `on-failure`.",
 			TypeSpec:    types.LocalType("SystemdServiceRestart"),
 		},
-		"restartSec": {
-			Description: "Optionally override the systemd service RestartSec. Defaults to `5`.",
-		},
-		"wantedBy": props.String("Optionally override the systemd service wanted-by. Defaults to `multi-user.target`."),
+		"restartSec": props.Integer("Optionally override the systemd service RestartSec. Defaults to `5`."),
+		"wantedBy":   props.String("Optionally override the systemd service wanted-by. Defaults to `multi-user.target`."),
 	}
 
 	requiredInputs := []string{"connection"}
 
 	outputs := map[string]schema.PropertySpec{
-		"ip": {
-			Description: "The IP address of the node.",
-			TypeSpec:    types.String,
-		},
 		"service": {
 			Description: "The remote systemd service.",
 			TypeSpec:    types.LocalResource("SystemdService", "remote"),
 		},
 	}
+	maps.Copy(outputs, inputs)
 
 	return schema.ResourceSpec{
 		IsComponent: true,
 		ObjectTypeSpec: schema.ObjectTypeSpec{
 			Description: "Etcd systemd service file. Will likely get replaced with a static function when https://github.com/pulumi/pulumi/issues/7583 gets resolved.",
 			Properties:  outputs,
-			Required:    append(requiredInputs, "ip", "service"),
+			Required:    append(requiredInputs, "service"),
 		},
 		InputProperties: inputs,
 		RequiredInputs:  requiredInputs,
