4b5339c288a63d1da0de2b7e209d265ad0398359
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-18 13:01:06 -0500

    Add new etcd install test

--- examples/examples_remote_test.go
+++ examples/examples_remote_test.go
@@ -48,3 +48,41 @@ func TestRemoteEtcdClusterTs(t *testing.T) {
 
 	integration.ProgramTest(t, &test)
 }
+
+func TestRemoteEtcdInstallTs(t *testing.T) {
+	const (
+		username = "root"
+		password = "root"
+	)
+
+	ctx := context.Background()
+	server, err := StartSshServer(ctx,
+		WithSshUsername(username),
+		WithSshPassword(password))
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+
+	port, err := server.Port(ctx)
+	assert.NoError(t, err)
+
+	defer StopSshServer(ctx, server) // TODO: Error handling?
+
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir:           path.Join(getCwd(t), "remote", "etcd-install-ts"),
+			Quick:         true,
+			SkipRefresh:   true,
+			RunUpdateTest: false,
+			Config: map[string]string{
+				"host":     "localhost",
+				"port":     port,
+				"user":     username,
+				"password": password,
+			},
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
---
+++ examples/remote/etcd-install-ts/index.ts
@@ -0,0 +1,18 @@
+import { Config } from '@pulumi/pulumi';
+import { EtcdInstall } from '@unmango/pulumi-kubernetes-the-hard-way/remote';
+import { Etcdctl } from '@unmango/pulumi-kubernetes-the-hard-way/tools';
+
+const config = new Config();
+const host = config.require('host');
+const port = config.requireNumber('port');
+const user = config.require('user');
+const password = config.require('password');
+
+const simple = new EtcdInstall('simple', {
+  connection: { host, port, user, password },
+});
+
+const etcdctl = new Etcdctl('simple', {
+  connection: simple.connection,
+  commands: 'member',
+});
