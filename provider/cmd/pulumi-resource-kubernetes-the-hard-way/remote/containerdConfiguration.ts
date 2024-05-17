16286d35984e7a45c7f3d233943c3b01dc390d45
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-17 13:12:58 -0500

    Refactor defaults and format as toml

--- provider/cmd/pulumi-resource-kubernetes-the-hard-way/remote/containerdConfiguration.ts
+++ provider/cmd/pulumi-resource-kubernetes-the-hard-way/remote/containerdConfiguration.ts
@@ -1,4 +1,4 @@
-import { ComponentResourceOptions, output } from '@pulumi/pulumi';
+import { ComponentResourceOptions, Output, interpolate, output } from '@pulumi/pulumi';
 import * as schema from '../schema-types';
 import { File } from './file';
 
@@ -9,11 +9,12 @@ export class ContainerdConfiguration extends schema.ContainerdConfiguration {
 
     const connection = output(args.connection);
     const cri = criDefaults(args.cri);
+    const path = output(args.path ?? '/etc/containerd');
 
     const file = new File(name, {
       connection,
-      content: '', // TODO
-      path: '', // TODO
+      content: formatAsToml(cri),
+      path,
     })
 
     this.connection = connection;;
@@ -24,13 +25,50 @@ export class ContainerdConfiguration extends schema.ContainerdConfiguration {
 
 function criDefaults(cri?: schema.ContainerdCriPluginConfigurationInputs): schema.ContainerdCriPluginConfigurationOutputs {
   return {
-    cni: {
-      binDir: output(cri?.cni.binDir ?? ''),
-      confDir: output(cri?.cni.confDir ?? ''),
-    },
-    containerd: {
-      defaultRuntimeName: output(cri?.containerd.defaultRuntimeName ?? ''),
-      snapshotter: output(cri?.containerd.snapshotter ?? ''),
-    },
+    cni: cniDefaults(cri?.cni),
+    containerd: containerdDefaults(cri?.containerd),
   };
+}
+
+function cniDefaults(cni?: schema.ContainerdCriPluginConfigurationCniInputs): schema.ContainerdCriPluginConfigurationCniOutputs {
+  return {
+    binDir: output(cni?.binDir ?? '/opt/cni/bin'),
+    confDir: output(cni?.confDir ?? '/etc/cni/net.d'),
+  };
+}
+
+function containerdDefaults(containerd?: schema.ContainerdCriPluginConfigurationContainerdInputs): schema.ContainerdCriPluginConfigurationContainerdOutputs {
+  return {
+    defaultRuntimeName: output(containerd?.defaultRuntimeName ?? 'runc'),
+    snapshotter: output(containerd?.snapshotter ?? 'overlayfs'),
+    runtimes: runcDefaults(containerd?.runtimes),
+  };
+}
+
+function runcDefaults(runtimes?: schema.ContainerdCriPluginConfigurationContainerdRuncInputs): schema.ContainerdCriPluginConfigurationContainerdRuncOutputs {
+  return {
+    options: {
+      systemdCgroup: output(runtimes?.options.systemdCgroup ?? true),
+    },
+    runtimeType: output(runtimes?.runtimeType ?? 'io.containerd.runc.v2'),
+  };
+}
+
+// https://github.com/kelseyhightower/kubernetes-the-hard-way/blob/master/configs/containerd-config.toml
+function formatAsToml(cri: schema.ContainerdCriPluginConfigurationOutputs): Output<string> {
+  return interpolate`# DO NOT MODIFY - Managed by Pulumi
+version = 2
+
+[plugins."io.containerd.grpc.v1.cri"]
+  [plugins."io.containerd.grpc.v1.cri".containerd]
+    snapshotter = "${cri.containerd.snapshotter}"
+    default_runtime_name = "${cri.containerd.defaultRuntimeName}"
+  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+    runtime_type = "${cri.containerd.runtimes?.runtimeType}"
+  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+    SystemdCgroup = ${cri.containerd.runtimes?.options.systemdCgroup}
+[plugins."io.containerd.grpc.v1.cri".cni]
+  bin_dir = "${cri.cni.binDir}"
+  conf_dir = "${cri.cni.confDir}"
+`;
 }
