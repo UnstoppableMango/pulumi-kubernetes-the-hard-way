2315c94eeab1b15dade8b40b60c8e1c9d59e9c23
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-16 19:45:48 -0500

    Fix it

--- examples/remote/cni-plugins-ts/index.ts
+++ examples/remote/cni-plugins-ts/index.ts
@@ -1,11 +1,20 @@
 import { Config } from '@pulumi/pulumi';
 import { CniBridgePluginConfiguration, CniLoopbackPluginConfiguration } from '@unmango/pulumi-kubernetes-the-hard-way/remote';
+import { Mkdir } from '@unmango/pulumi-kubernetes-the-hard-way/tools';
 
 const config = new Config();
 const host = config.require('host');
 const port = config.requireNumber('port');
 const user = config.require('user');
 const password = config.require('password');
+
+const mkdir = new Mkdir('var-lib', {
+  connection: { host, port, user, password },
+  create: {
+    parents: true,
+    directory: '/var/lib/kubernetes',
+  },
+});
 
 const bridgeConfig = new CniBridgePluginConfiguration('simple', {
   connection: { host, port, user, password },
