0b4d0ad42aabc014b5440622f59e53ae8dc0e00f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-18 00:31:10 -0500

    Remove the old test and program (#252)

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -58,62 +58,6 @@ func TestRemoteInstallTs(t *testing.T) {
 				"password": password,
 				"basePath": "/config",
 			},
-			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-			},
-		})
-
-	integration.ProgramTest(t, &test)
-}
-
-func TestRemoteTs(t *testing.T) {
-	t.Skip("Disabling in favor of future, more specific, tests")
-
-	const (
-		username = "root"
-		password = "root"
-		content  = "Some content idk"
-	)
-
-	node := newNode(t,
-		WithSshUsername(username),
-		WithSshPassword(password),
-	)
-
-	// err = server.CopyFile(ctx,
-	// 	path.Join(getCwd(t), "testdata", "text-file.tar.gz"),
-	// 	path.Join("/config", "text-file.tar.gz"))
-	// assert.NoError(t, err)
-
-	test := getJSBaseOptions(t).
-		With(integration.ProgramTestOptions{
-			Dir:           path.Join(getCwd(t), "remote-ts"),
-			Quick:         true,
-			SkipRefresh:   true,
-			RunUpdateTest: false,
-			Config: map[string]string{
-				"host":     "localhost",
-				"port":     node.Port,
-				"user":     username,
-				"password": password,
-				"content":  content,
-				"basePath": "/config",
-			},
-			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-				assert.Equal(t, content, stack.Outputs["fileStdout"])
-				assert.Empty(t, stack.Outputs["fileStderr"])
-
-				// Wget outputs progress to stderr
-				assert.Empty(t, stack.Outputs["wgetStdout"])
-				assert.NotEmpty(t, stack.Outputs["wgetStderr"])
-
-				data := node.ReadFile(t, "/config/index.html")
-				assert.Equal(t, exampleComHtml, data)
-
-				// assert.Empty(t, stack.Outputs["tarStdout"])
-				// assert.Empty(t, stack.Outputs["tarStderr"])
-
-				assert.NotEmpty(t, stack.Outputs["mktemp"])
-			},
 		})
 
 	integration.ProgramTest(t, &test)
