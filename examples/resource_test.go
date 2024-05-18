7dc537f4d5db5b58da81ea4b5d9a5eaa3fece629
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-17 23:04:12 -0500

    Get the "framework" basically figured out

--- examples/resource_test.go
+++ examples/resource_test.go
@@ -14,10 +14,8 @@ import (
 
 type ResourceValidator func(t *testing.T, res apitype.ResourceV3)
 
-type validatorToken struct {
-	Type      tokens.Type
-	Name      string
-	Validator ResourceValidator
+type ResourceContext struct {
+	tokens map[validatorKey]ResourceValidator
 }
 
 type validatorKey struct {
@@ -25,23 +23,40 @@ type validatorKey struct {
 	Name string
 }
 
-func Validate(typ tokens.Type, name string, validator ResourceValidator) validatorToken {
-	return validatorToken{Type: typ, Name: name, Validator: validator}
+func Validate(ctx *ResourceContext, typ tokens.Type, name string, validator ResourceValidator) {
+	ctx.tokens[validatorKey{Type: typ, Name: name}] = validator
 }
 
-func ResourceValidation(validators ...validatorToken) integration.ProgramTestOptions {
-	return integration.ProgramTestOptions{
+func ResourceTest(t *testing.T, project string, baseOptions integration.ProgramTestOptions, validation func(ctx *ResourceContext)) {
+	skipIfShort(t)
+
+	const (
+		username = "root"
+		password = "root"
+	)
+
+	node := newNode(t,
+		WithSshUsername(username),
+		WithSshPassword(password),
+	)
+
+	test := baseOptions.With(integration.ProgramTestOptions{
+		Dir: path.Join(getCwd(t), project),
+		Config: map[string]string{
+			"host":     "localhost",
+			"port":     node.Port,
+			"user":     username,
+			"password": password,
+		},
 		ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-			lookup := map[validatorKey]ResourceValidator{}
-			for _, token := range validators {
-				key := validatorKey{Type: token.Type, Name: token.Name}
-				lookup[key] = token.Validator
-			}
+			ctx := ResourceContext{tokens: map[validatorKey]ResourceValidator{}}
+			validation(&ctx)
+			lookup := ctx.tokens
 
 			validated := []validatorKey{}
 			for _, res := range stack.Deployment.Resources {
 				key := validatorKey{Type: res.Type, Name: res.URN.Name()}
-				testName := fmt.Sprintf("Testing resource %s", res.URN)
+				testName := fmt.Sprintf("Testing resource %s", res.URN.Name())
 				if validator, ok := lookup[key]; ok && t.Run(testName, func(t *testing.T) {
 					validator(t, res)
 				}) {
@@ -51,129 +66,7 @@ func ResourceValidation(validators ...validatorToken) integration.ProgramTestOpt
 
 			assert.ElementsMatch(t, maps.Keys(lookup), validated)
 		},
-	}
-}
-
-func ResourceTest(t *testing.T) {
-	validateBridge := func(t *testing.T, res apitype.ResourceV3) {
-		assert.NotEmpty(t, res.Outputs)
-
-		bridge, ok := res.Outputs["bridge"]
-		assert.True(t, ok, "Output `bridge` was not set")
-		assert.Equal(t, "cni0", bridge)
-
-		name, ok := res.Outputs["name"]
-		assert.True(t, ok, "Output `name` was not set")
-		assert.Equal(t, "bridge", name)
-
-		typ, ok := res.Outputs["type"]
-		assert.True(t, ok, "Output `type` was not set")
-		assert.Equal(t, "bridge", typ)
-
-		cniVersion, ok := res.Outputs["cniVersion"]
-		assert.True(t, ok, "Output `cniVersion` was not set")
-		assert.Equal(t, "1.0.0", cniVersion)
-
-		path, ok := res.Outputs["path"]
-		assert.True(t, ok, "Output `path` was not set")
-		assert.Equal(t, "/etc/cni/net.d/10-bridge.conf", path)
-
-		subnet, ok := res.Outputs["subnet"]
-		assert.True(t, ok, "Output `subnet` was not set")
-		assert.Equal(t, "10.0.69.0/24", subnet)
-
-		isGateway, ok := res.Outputs["isGateway"]
-		assert.True(t, ok, "Output `isGateway` was not set")
-		assert.Equal(t, true, isGateway)
-
-		ipMasq, ok := res.Outputs["ipMasq"]
-		assert.True(t, ok, "Output `ipMasq` was not set")
-		assert.Equal(t, true, ipMasq)
-
-		assert.Contains(t, res.Outputs, "file")
-		assert.Contains(t, res.Outputs, "ipam")
-	}
-
-	validateLoopback := func(t *testing.T, res apitype.ResourceV3) {
-		assert.NotEmpty(t, res.Outputs)
-
-		cniVersion, ok := res.Outputs["cniVersion"]
-		assert.True(t, ok, "Output `cniVersion` was not set")
-		assert.Equal(t, "1.1.0", cniVersion)
-
-		name, ok := res.Outputs["name"]
-		assert.True(t, ok, "Output `name` was not set")
-		assert.Equal(t, "lo", name)
-
-		path, ok := res.Outputs["path"]
-		assert.True(t, ok, "Output `path` was not set")
-		assert.Equal(t, "/etc/cni/net.d/99-loopback.conf", path)
-
-		typ, ok := res.Outputs["type"]
-		assert.True(t, ok, "Output `type` was not set")
-		assert.Equal(t, "loopback", typ)
-
-		assert.Contains(t, res.Outputs, "file")
-	}
-
-	validatePlugins := func(t *testing.T, res apitype.ResourceV3) {
-		assert.NotEmpty(t, res.Outputs)
-
-		subnet, ok := res.Outputs["subnet"]
-		assert.True(t, ok, "Output `subnet` was not set")
-		assert.Equal(t, "10.0.69.0/24", subnet)
-
-		directory, ok := res.Outputs["directory"]
-		assert.True(t, ok, "Output `directory` was not set")
-		assert.Equal(t, "/etc/cni2/net.d", directory)
-
-		assert.Contains(t, res.Outputs, "bridge")
-		assert.Contains(t, res.Outputs, "connection")
-		assert.Contains(t, res.Outputs, "loopback")
-		assert.Contains(t, res.Outputs, "mkdir")
-	}
-
-	test := getJSBaseOptions(t).
-		With(integration.ProgramTestOptions{
-			Dir: path.Join(getCwd(t), "remote", "cni-plugins-ts"),
-			Config: map[string]string{
-				"host":     "localhost",
-				"port":     node.Port,
-				"user":     username,
-				"password": password,
-			},
-			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-				validatedResources := []string{}
-				for _, res := range stack.Deployment.Resources {
-					switch res.Type {
-					case "kubernetes-the-hard-way:remote:CniBridgePluginConfiguration":
-						switch res.URN.Name() {
-						case "simple":
-							validateBridge(t, res)
-							validatedResources = append(validatedResources, "simple")
-						}
-					case "kubernetes-the-hard-way:remote:CniLoopbackPluginConfiguration":
-						switch res.URN.Name() {
-						case "simple":
-							validateLoopback(t, res)
-							validatedResources = append(validatedResources, "simple")
-						}
-					case "kubernetes-the-hard-way:remote:CniPluginConfiguration":
-						switch res.URN.Name() {
-						case "all":
-							validatePlugins(t, res)
-							validatedResources = append(validatedResources, "all")
-						}
-					}
-				}
-
-				assert.ElementsMatch(t, []string{
-					"simple",
-					"simple",
-					"all",
-				}, validatedResources, "Not all resources were validated")
-			},
-		})
+	})
 
 	integration.ProgramTest(t, &test)
 }
