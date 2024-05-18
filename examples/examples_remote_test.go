9523a28a5cdcded53633aa514e8875b20a340ff8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-17 23:06:18 -0500

    Onboard the etcd install test

--- examples/examples_remote_test.go
+++ examples/examples_remote_test.go
@@ -301,91 +301,34 @@ func TestRemoteEtcdClusterSingleTs(t *testing.T) {
 }
 
 func TestRemoteEtcdInstallTs(t *testing.T) {
-	skipIfShort(t)
+	options := getJSBaseOptions(t).With(integration.ProgramTestOptions{
+		SkipPreview:   true,
+		RunUpdateTest: false,
+		ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+			etcdctlVersion, ok := stack.Outputs["etcdctlVersion"]
+			assert.True(t, ok, "Output `etcdctlVersion` was not set")
+			assert.Equal(t, "etcdctl version: 3.4.15\nAPI version: 3.4", etcdctlVersion) // TODO: No hardcoded versions
+		},
+	})
 
-	const (
-		username = "root"
-		password = "root"
-	)
+	ResourceTest(t, "remote/etcd-install-ts", options, func(ctx *ResourceContext) {
+		Validate(ctx, "kubernetes-the-hard-way:remote:EtcdInstall", "simple", func(t *testing.T, res apitype.ResourceV3) {
+			assert.NotEmpty(t, res.Outputs)
 
-	node := newNode(t,
-		WithSshUsername(username),
-		WithSshPassword(password),
-	)
+			expectOutput(t, res, "architecture", "amd64")
+			expectOutput(t, res, "archiveName", "etcd-v3.4.15-linux-amd64.tar.gz")
+			expectOutput(t, res, "directory", "/usr/local/bin")
+			expectOutput(t, res, "etcdPath", "/usr/local/bin/etcd")
+			expectOutput(t, res, "etcdctlPath", "/usr/local/bin/etcdctl")
+			expectOutput(t, res, "name", "simple")
+			expectOutput(t, res, "url", "https://github.com/etcd-io/etcd/releases/download/v3.4.15/etcd-v3.4.15-linux-amd64.tar.gz")
+			expectOutput(t, res, "version", "3.4.15")
 
-	validateSimple := func(t *testing.T, res apitype.ResourceV3) {
-		assert.NotEmpty(t, res.Outputs)
-
-		arch, ok := res.Outputs["architecture"]
-		assert.True(t, ok, "Output `architecture` was not set")
-		assert.Equal(t, "amd64", arch)
-
-		archiveName, ok := res.Outputs["archiveName"]
-		assert.True(t, ok, "Output `archiveName` was not set")
-		assert.Equal(t, "etcd-v3.4.15-linux-amd64.tar.gz", archiveName)
-
-		directory, ok := res.Outputs["directory"]
-		assert.True(t, ok, "Output `directory` was not set")
-		assert.Equal(t, "/usr/local/bin", directory)
-
-		etcdPath, ok := res.Outputs["etcdPath"]
-		assert.True(t, ok, "Output `etcdPath` was not set")
-		assert.Equal(t, "/usr/local/bin/etcd", etcdPath)
-
-		etcdctlPath, ok := res.Outputs["etcdctlPath"]
-		assert.True(t, ok, "Output `etcdctlPath` was not set")
-		assert.Equal(t, "/usr/local/bin/etcdctl", etcdctlPath)
-
-		name, ok := res.Outputs["name"]
-		assert.True(t, ok, "Output `name` was not set")
-		assert.Equal(t, "simple", name)
-
-		url, ok := res.Outputs["url"]
-		assert.True(t, ok, "Output `url` was not set")
-		assert.Equal(t, "https://github.com/etcd-io/etcd/releases/download/v3.4.15/etcd-v3.4.15-linux-amd64.tar.gz", url)
-
-		version, ok := res.Outputs["version"]
-		assert.True(t, ok, "Output `version` was not set")
-		assert.Equal(t, "3.4.15", version)
-
-		assert.Contains(t, res.Outputs, "download")
-		assert.Contains(t, res.Outputs, "mkdir")
-		assert.Contains(t, res.Outputs, "mvEtcd")
-		assert.Contains(t, res.Outputs, "mvEtcdctl")
-		assert.Contains(t, res.Outputs, "tar")
-	}
-
-	test := getJSBaseOptions(t).
-		With(integration.ProgramTestOptions{
-			Dir:           path.Join(getCwd(t), "remote", "etcd-install-ts"),
-			SkipPreview:   true,  // TODO: There is some bug surrounding this
-			RunUpdateTest: false, // TODO: Enable
-			Config: map[string]string{
-				"host":     "localhost",
-				"port":     node.Port,
-				"user":     username,
-				"password": password,
-			},
-			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-				etcdctlVersion, ok := stack.Outputs["etcdctlVersion"]
-				assert.True(t, ok, "Output `etcdctlVersion` was not set")
-				assert.Equal(t, "etcdctl version: 3.4.15\nAPI version: 3.4", etcdctlVersion) // TODO: No hardcoded versions
-
-				validatedResources := []string{}
-				for _, res := range stack.Deployment.Resources {
-					if res.Type != "kubernetes-the-hard-way:remote:EtcdInstall" {
-						continue
-					}
-					switch res.URN.Name() {
-					case "simple":
-						validateSimple(t, res)
-						validatedResources = append(validatedResources, "simple")
-					}
-				}
-
-				assert.Equal(t, []string{"simple"}, validatedResources, "Not all resources were validated")
-			},
+			assert.Contains(t, res.Outputs, "download")
+			assert.Contains(t, res.Outputs, "mkdir")
+			assert.Contains(t, res.Outputs, "mvEtcd")
+			assert.Contains(t, res.Outputs, "mvEtcdctl")
+			assert.Contains(t, res.Outputs, "tar")
 		})
-
-	integration.ProgramTest(t, &test)
+	})
 }
