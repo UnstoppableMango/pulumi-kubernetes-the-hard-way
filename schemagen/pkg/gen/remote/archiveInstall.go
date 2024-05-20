38157e04eb7b93b82f71dbf47f14afef5f1ce1c7
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-20 11:29:28 -0500

    More splitting

---
+++ schemagen/pkg/gen/remote/archiveInstall.go
@@ -0,0 +1,107 @@
+package remote
+
+import (
+	"fmt"
+	"maps"
+
+	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/props"
+	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/types"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+)
+
+func generateArchiveInstall(
+	commandSpec schema.PackageSpec,
+	typ, description string,
+	files ...string,
+) schema.PackageSpec {
+	inputs := map[string]schema.PropertySpec{
+		"architecture": {
+			Description: "The CPU architecture to install.",
+			TypeSpec:    types.LocalType("Architecture", "remote"),
+		},
+		"connection": {
+			Description: "The parameters with which to connect to the remote host.",
+			TypeSpec:    types.ExtType(commandSpec, "Connection", "remote"),
+		},
+		"directory": {
+			Description: "The directory to install the binary to.",
+			TypeSpec:    types.String,
+			Default:     "/usr/local/bin",
+		},
+		"version": props.String("The version to install."),
+	}
+
+	requiredInputs := []string{"connection"}
+
+	outputs := map[string]schema.PropertySpec{
+		"archiveName": props.String("The name of the downloaded archive."),
+		"download": {
+			Description: "The download operation.",
+			TypeSpec:    types.LocalResource("Download", "remote"),
+		},
+		"mkdir": {
+			Description: "The mkdir operation.",
+			TypeSpec:    types.LocalResource("Mkdir", "tools"),
+		},
+		"mktemp": {
+			Description: "The mktemp operation.",
+			TypeSpec:    types.LocalResource("Mktemp", "tools"),
+		},
+		"path": props.String("The path to the installed binary."),
+		"rm": {
+			Description: "The rm operation.",
+			TypeSpec:    types.LocalResource("Rm", "tools"),
+		},
+		"tar": {
+			Description: "The tar operation.",
+			TypeSpec:    types.LocalResource("Tar", "tools"),
+		},
+		"url": props.String("The url used to download the binary."),
+	}
+	maps.Copy(outputs, inputs)
+
+	requiredOutputs := []string{
+		"architecture",
+		"archiveName",
+		"connection",
+		"download",
+		"directory",
+		"mkdir",
+		"mktemp",
+		"rm",
+		"tar",
+		"url",
+		"version",
+	}
+
+	for _, f := range files {
+		mvProp := f + "Mv"
+		outputs[mvProp] = schema.PropertySpec{
+			Description: fmt.Sprintf("The %s mv operation.", f),
+			TypeSpec:    types.LocalResource("Mv", "tools"),
+		}
+		pathProp := f + "Path"
+		outputs[pathProp] = schema.PropertySpec{
+			Description: fmt.Sprintf("The %s path on the remote system", f),
+			TypeSpec:    types.String,
+		}
+		requiredOutputs = append(requiredOutputs, mvProp, pathProp)
+	}
+
+	return schema.PackageSpec{
+		Types:     map[string]schema.ComplexTypeSpec{},
+		Functions: map[string]schema.FunctionSpec{},
+		Resources: map[string]schema.ResourceSpec{
+			name(typ): {
+				IsComponent: true,
+				ObjectTypeSpec: schema.ObjectTypeSpec{
+					Description: description,
+					Properties:  outputs,
+					Required:    requiredOutputs,
+				},
+				InputProperties: inputs,
+				RequiredInputs:  requiredInputs,
+			},
+		},
+	}
+}
--- schemagen/pkg/gen/remote/index.go
+++ schemagen/pkg/gen/remote/index.go
@@ -9,26 +9,10 @@ func Generate(commandSpec schema.PackageSpec) schema.PackageSpec {
 	functions := map[string]schema.FunctionSpec{}
 
 	resources := map[string]schema.ResourceSpec{
-		name("CniPluginsInstall"): generateArchiveInstall(commandSpec, "Installs cni-plugins on a remote system",
-			"bandwidth", "bridge", "dhcp", "dummy", "firewall", "hostDevice",
-			"hostLocal", "ipvlan", "loopback", "macvlan", "portmap", "ptp", "sbr",
-			"static", "tap", "tuning", "vlan", "vrf"),
 		name("ContainerdConfiguration"):        generateContainerdConfiguration(commandSpec),
-		name("ContainerdInstall"):              generateArchiveInstall(commandSpec, "Installs containerd on a remote system", "containerd"),
 		name("CniBridgePluginConfiguration"):   generateCniBridgePluginConfiguration(commandSpec),
 		name("CniLoopbackPluginConfiguration"): generateCniLoopbackPluginConfiguration(commandSpec),
 		name("CniPluginConfiguration"):         generateCniPluginConfiguration(commandSpec),
-		name("CrictlInstall"):                  generateArchiveInstall(commandSpec, "Installs crictl on a remote system", "crictl"),
-		name("EtcdInstall"):                    generateArchiveInstall(commandSpec, "Installs etcd on a remote system", "etcd", "etcdctl"),
-		name("KubeApiServerInstall"):           generateBinaryInstall(commandSpec, "Installs kube-apiserver on a remote system."),
-		name("KubeControllerManagerInstall"):   generateBinaryInstall(commandSpec, "Installs kube-controller-manager on a remote system."),
-		name("KubectlInstall"):                 generateBinaryInstall(commandSpec, "Installs kubectl on a remote system."),
-		name("KubeletInstall"):                 generateBinaryInstall(commandSpec, "Installs kubelet on a remote system."),
-		name("KubeProxyInstall"):               generateBinaryInstall(commandSpec, "Installs kube-proxy on a remote system."),
-		name("KubeSchedulerInstall"):           generateBinaryInstall(commandSpec, "Installs kube-scheduler on a remote system."),
-		name("RuncInstall"):                    generateBinaryInstall(commandSpec, "Installs runc on a remote system."),
-		name("StaticPod"):                      generateStaticPod(commandSpec),
-		name("SystemdService"):                 generateSystemdService(commandSpec),
 	}
 
 	base := schema.PackageSpec{
@@ -50,5 +34,21 @@ func Generate(commandSpec schema.PackageSpec) schema.PackageSpec {
 		generateStartSystemdService(commandSpec, "StartEtcd", "etcd"),
 		generateStartSystemdService(commandSpec, "StartKubelet", "kubelet"),
 		generateStartSystemdService(commandSpec, "StartKubeProxy", "kube-proxy"),
+		generateStaticPod(commandSpec),
+		generateSystemdService(commandSpec),
+		generateArchiveInstall(commandSpec, "CniPluginsInstall", "Installs cni-plugins on a remote system",
+			"bandwidth", "bridge", "dhcp", "dummy", "firewall", "hostDevice",
+			"hostLocal", "ipvlan", "loopback", "macvlan", "portmap", "ptp", "sbr",
+			"static", "tap", "tuning", "vlan", "vrf"),
+		generateArchiveInstall(commandSpec, "ContainerdInstall", "Installs containerd on a remote system", "containerd"),
+		generateArchiveInstall(commandSpec, "CrictlInstall", "Installs crictl on a remote system", "crictl"),
+		generateArchiveInstall(commandSpec, "EtcdInstall", "Installs etcd on a remote system", "etcd", "etcdctl"),
+		generateBinaryInstall(commandSpec, "KubeApiServerInstall", "Installs kube-apiserver on a remote system."),
+		generateBinaryInstall(commandSpec, "KubeControllerManagerInstall", "Installs kube-controller-manager on a remote system."),
+		generateBinaryInstall(commandSpec, "KubectlInstall", "Installs kubectl on a remote system."),
+		generateBinaryInstall(commandSpec, "KubeletInstall", "Installs kubelet on a remote system."),
+		generateBinaryInstall(commandSpec, "KubeProxyInstall", "Installs kube-proxy on a remote system."),
+		generateBinaryInstall(commandSpec, "KubeSchedulerInstall", "Installs kube-scheduler on a remote system."),
+		generateBinaryInstall(commandSpec, "RuncInstall", "Installs runc on a remote system."),
 	)
 }
