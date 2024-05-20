7490a453ccbe74ac1fe2d84c08c0ad9f9c36b5a3
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-20 12:59:58 -0500

    Rebase conflicts and remove types file

--- schemagen/pkg/gen/remote/schema.go
+++ schemagen/pkg/gen/remote/schema.go
@@ -1,42 +1,56 @@
 package remote
 
 import (
+	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/internal"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 )
 
 func Generate(commandSpec schema.PackageSpec) schema.PackageSpec {
-	return schema.PackageSpec{
+	base := schema.PackageSpec{
 		Functions: map[string]schema.FunctionSpec{},
-		Types:     generateTypes(commandSpec),
-		Resources: map[string]schema.ResourceSpec{
-			name("CniPluginsInstall"): generateArchiveInstall(commandSpec, "Installs cni-plugins on a remote system",
-				"bandwidth", "bridge", "dhcp", "dummy", "firewall", "hostDevice",
-				"hostLocal", "ipvlan", "loopback", "macvlan", "portmap", "ptp", "sbr",
-				"static", "tap", "tuning", "vlan", "vrf"),
-			name("ContainerdInstall"):            generateArchiveInstall(commandSpec, "Installs containerd on a remote system", "containerd"),
-			name("CrictlInstall"):                generateArchiveInstall(commandSpec, "Installs crictl on a remote system", "crictl"),
-			name("Download"):                     generateDownload(commandSpec),
-			name("EtcdCluster"):                  generateEtcdCluster(),
-			name("EtcdConfiguration"):            generateEtcdConfiguration(commandSpec),
-			name("EtcdInstall"):                  generateArchiveInstall(commandSpec, "Installs etcd on a remote system", "etcd", "etcdctl"),
-			name("EtcdService"):                  generateEtcdService(commandSpec),
-			name("File"):                         generateFile(commandSpec),
-			name("KubeApiServerInstall"):         generateBinaryInstall(commandSpec, "Installs kube-apiserver on a remote system."),
-			name("KubeControllerManagerInstall"): generateBinaryInstall(commandSpec, "Installs kube-controller-manager on a remote system."),
-			name("KubectlInstall"):               generateBinaryInstall(commandSpec, "Installs kubectl on a remote system."),
-			name("KubeletInstall"):               generateBinaryInstall(commandSpec, "Installs kubelet on a remote system."),
-			name("KubeletService"):               generateKubeletService(commandSpec),
-			name("KubeProxyInstall"):             generateBinaryInstall(commandSpec, "Installs kube-proxy on a remote system."),
-			name("KubeProxyService"):             generateKubeProxyService(commandSpec),
-			name("KubeSchedulerInstall"):         generateBinaryInstall(commandSpec, "Installs kube-scheduler on a remote system."),
-			name("RuncInstall"):                  generateBinaryInstall(commandSpec, "Installs runc on a remote system."),
-			name("ProvisionEtcd"):                generateProvisionEtcd(commandSpec),
-			name("StartContainerd"):              generateStartSystemdService(commandSpec, "containerd"),
-			name("StartEtcd"):                    generateStartSystemdService(commandSpec, "etcd"),
-			name("StartKubelet"):                 generateStartSystemdService(commandSpec, "kubelet"),
-			name("StartKubeProxy"):               generateStartSystemdService(commandSpec, "kube-proxy"),
-			name("StaticPod"):                    generateStaticPod(commandSpec),
-			name("SystemdService"):               generateSystemdService(commandSpec),
+		Resources: map[string]schema.ResourceSpec{},
+		Types: map[string]schema.ComplexTypeSpec{
+			name("Architecture"): {
+				ObjectTypeSpec: schema.ObjectTypeSpec{
+					Description: "CPU architecture",
+					Type:        "string",
+				},
+				Enum: []schema.EnumValueSpec{
+					{Value: "amd64"},
+					{Value: "arm64"},
+				},
+			},
 		},
 	}
+
+	return internal.ExtendSchemas(base,
+		generateDownload(commandSpec),
+		generateEtcdCluster(commandSpec),
+		generateEtcdConfiguration(commandSpec),
+		generateEtcdService(commandSpec),
+		generateFile(commandSpec),
+		generateKubeletService(commandSpec),
+		generateKubeProxyService(commandSpec),
+		generateProvisionEtcd(commandSpec),
+		generateStartSystemdService(commandSpec, "StartContainerd", "containerd"),
+		generateStartSystemdService(commandSpec, "StartEtcd", "etcd"),
+		generateStartSystemdService(commandSpec, "StartKubelet", "kubelet"),
+		generateStartSystemdService(commandSpec, "StartKubeProxy", "kube-proxy"),
+		generateStaticPod(commandSpec),
+		generateSystemdService(commandSpec),
+		generateArchiveInstall(commandSpec, "CniPluginsInstall", "Installs cni-plugins on a remote system",
+			"bandwidth", "bridge", "dhcp", "dummy", "firewall", "hostDevice",
+			"hostLocal", "ipvlan", "loopback", "macvlan", "portmap", "ptp", "sbr",
+			"static", "tap", "tuning", "vlan", "vrf"),
+		generateArchiveInstall(commandSpec, "ContainerdInstall", "Installs containerd on a remote system", "containerd"),
+		generateArchiveInstall(commandSpec, "CrictlInstall", "Installs crictl on a remote system", "crictl"),
+		generateArchiveInstall(commandSpec, "EtcdInstall", "Installs etcd on a remote system", "etcd", "etcdctl"),
+		generateBinaryInstall(commandSpec, "KubeApiServerInstall", "Installs kube-apiserver on a remote system."),
+		generateBinaryInstall(commandSpec, "KubeControllerManagerInstall", "Installs kube-controller-manager on a remote system."),
+		generateBinaryInstall(commandSpec, "KubectlInstall", "Installs kubectl on a remote system."),
+		generateBinaryInstall(commandSpec, "KubeletInstall", "Installs kubelet on a remote system."),
+		generateBinaryInstall(commandSpec, "KubeProxyInstall", "Installs kube-proxy on a remote system."),
+		generateBinaryInstall(commandSpec, "KubeSchedulerInstall", "Installs kube-scheduler on a remote system."),
+		generateBinaryInstall(commandSpec, "RuncInstall", "Installs runc on a remote system."),
+	)
 }
