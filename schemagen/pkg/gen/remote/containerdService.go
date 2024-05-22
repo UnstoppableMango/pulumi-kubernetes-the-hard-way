d124009ea966c2e4b15d634bea8d78abb077022c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-22 16:35:47 -0500

    Add containerd service
    Somehow I missed this?

---
+++ schemagen/pkg/gen/remote/containerdService.go
@@ -0,0 +1,55 @@
+package remote
+
+import (
+	"maps"
+
+	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/props"
+	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/types"
+	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+)
+
+func generateContainerdService(commandSpec schema.PackageSpec) schema.PackageSpec {
+	inputs := map[string]schema.PropertySpec{
+		"configuration": {
+			Description: "Containerd configuration.",
+			TypeSpec:    types.LocalType("ContainerdConfigurationProps", "remote"),
+		},
+		"connection":    props.Connection(commandSpec),
+		"description":   props.String("Optional systemd unit description."),
+		"directory":     props.String("The location to create the service file."),
+		"documentation": props.String("Optional systemd unit documentation"),
+		"restart": {
+			Description: "Optionally override the systemd service restart behaviour. Defaults to `on-failure`.",
+			TypeSpec:    types.LocalType("SystemdServiceRestart", "remote"),
+		},
+		"restartSec": props.String("Optionally override the systemd service RestartSec. Defaults to `5`."),
+		"wantedBy":   props.String("Optionally override the systemd service wanted-by. Defaults to `multi-user.target`."),
+	}
+
+	requiredInputs := []string{"configuration", "connection"}
+
+	outputs := map[string]schema.PropertySpec{
+		"service": {
+			Description: "The remote systemd service.",
+			TypeSpec:    types.LocalResource("SystemdService", "remote"),
+		},
+	}
+	maps.Copy(outputs, inputs)
+
+	return schema.PackageSpec{
+		Types:     map[string]schema.ComplexTypeSpec{},
+		Functions: map[string]schema.FunctionSpec{},
+		Resources: map[string]schema.ResourceSpec{
+			name("ContainerdService"): {
+				IsComponent: true,
+				ObjectTypeSpec: schema.ObjectTypeSpec{
+					Description: "Containerd systemd service file. Will likely get replaced with a static function when https://github.com/pulumi/pulumi/issues/7583 gets resolved.",
+					Properties:  outputs,
+					Required:    append(requiredInputs, "service"),
+				},
+				InputProperties: inputs,
+				RequiredInputs:  requiredInputs,
+			},
+		},
+	}
+}
