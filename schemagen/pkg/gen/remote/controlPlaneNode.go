b9b84e25bf816461fe58144ed33922155eb116be
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-25 18:36:33 -0500

    WIP on the controlplane node

--- schemagen/pkg/gen/remote/controlPlaneNode.go
+++ schemagen/pkg/gen/remote/controlPlaneNode.go
@@ -5,20 +5,71 @@ import (
 	"slices"
 
 	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/props"
+	"github.com/UnstoppableMango/pulumi-kubernetes-the-hard-way/schemagen/pkg/gen/types"
 	"github.com/pulumi/pulumi/pkg/v3/codegen/schema"
 )
 
 func generateControlPlaneNode(commandSpec schema.PackageSpec) schema.PackageSpec {
 	inputs := map[string]schema.PropertySpec{
-		"connection": props.Connection(commandSpec),
+		"caCertificatePath":              props.String("The path to the root certificate authority certificate."),
+		"caPrivateKeyPath":               props.String("The path to the root certificate authority private key."),
+		"connection":                     props.Connection(commandSpec),
+		"encryptionConfig":               props.String("The v1/EncryptionConfig yaml."),
+		"kubeApiServerCertificatePath":   props.String("The path to the kube-apiserver certificate."),
+		"kubeApiServerPrivateKeyPath":    props.String("The path to the kube-apiserver private key."),
+		"serviceAccountsCertificatePath": props.String("The path to the service accounts certificate."),
+		"serviceAccountsPrivateKeyPath":  props.String("The path to the service accounts private key."),
 	}
 
 	requiredInputs := []string{"connection"}
 
-	outputs := map[string]schema.PropertySpec{}
+	outputs := map[string]schema.PropertySpec{
+		"kubeApiServerInstall": {
+			Description: "The kube-apiserver install.",
+			TypeSpec:    types.LocalResource("KubeApiServerInstall", "remote"),
+		},
+		"kubeApiServerService": {
+			Description: "The kube-apiserver systemd service.",
+			TypeSpec:    types.LocalResource("KubeApiServerService", "remote"),
+		},
+		"kubeControllerManagerInstall": {
+			Description: "The kube-controller-manager install.",
+			TypeSpec:    types.LocalResource("KubeControllerManagerInstall", "remote"),
+		},
+		"kubeControllerManagerService": {
+			Description: "The kube-controller-manager systemd service.",
+			TypeSpec:    types.LocalResource("KubeControllerManagerService", "remote"),
+		},
+		"kubectlInstall": {
+			Description: "The kubectl install.",
+			TypeSpec:    types.LocalResource("KubectlInstall", "remote"),
+		},
+		"kubernetesConfigurationMkdir": {
+			Description: "The kubernetes configuration mkdir operation.",
+			TypeSpec:    types.LocalResource("Mkdir", "tools"),
+		},
+		"kubeSchedulerInstall": {
+			Description: "The kube-scheduler isntall.",
+			TypeSpec:    types.LocalResource("KubeSchedulerInstall", "remote"),
+		},
+		"kubeSchedulerService": {
+			Description: "The kube-scheduler systemd service.",
+		},
+		"varLibKubernetesMkdir": {
+			Description: "The /var/lib/kubernetes mkdir operation.",
+			TypeSpec:    types.LocalResource("Mkdir", "tools"),
+		},
+	}
 	maps.Copy(outputs, inputs)
 
-	requiredOutputs := slices.Concat(requiredInputs, []string{})
+	requiredOutputs := slices.Concat(requiredInputs, []string{
+		"kubeApiServerInstall",
+		"kubeControllerManagerInstall",
+		"kubectlInstall",
+		"kubernetesConfigurationMkdir",
+		"kubeSchedulerInstall",
+		"varLibKubernetesMkdir",
+	})
 
 	return schema.PackageSpec{
 		Types:     map[string]schema.ComplexTypeSpec{},
