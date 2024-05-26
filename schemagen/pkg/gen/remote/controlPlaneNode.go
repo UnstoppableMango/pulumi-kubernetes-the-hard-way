2b4627b36c916b6960137604ec9784b82c13046b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-25 23:42:49 -0500

    More stuff for the resource

--- schemagen/pkg/gen/remote/controlPlaneNode.go
+++ schemagen/pkg/gen/remote/controlPlaneNode.go
@@ -11,22 +11,55 @@ import (
 
 func generateControlPlaneNode(commandSpec schema.PackageSpec) schema.PackageSpec {
 	inputs := map[string]schema.PropertySpec{
-		"caCertificatePath":                   props.String("The path to the root certificate authority certificate."),
-		"caPrivateKeyPath":                    props.String("The path to the root certificate authority private key."),
-		"connection":                          props.Connection(commandSpec),
-		"encryptionConfigYaml":                props.String("The v1/EncryptionConfig yaml."),
-		"kubeApiServerCertificatePath":        props.String("The path to the kube-apiserver certificate."),
-		"kubeApiServerPrivateKeyPath":         props.String("The path to the kube-apiserver private key."),
-		"kubeControllerManagerKubeconfigPath": props.String("The path to the kube-controller-manager kubeconfig file."),
-		"kubeSchedulerConfigYaml":             props.String("The kube-scheduler config yaml."),
-		"kubeSchedulerKubeconfigPath":         props.String("The path to the kube-scheduler kubeconfig file."),
-		"serviceAccountsCertificatePath":      props.String("The path to the service accounts certificate."),
-		"serviceAccountsPrivateKeyPath":       props.String("The path to the service accounts private key."),
+		"apiServerCount": props.Integer("The number of kube-apiserver instance."),
+		"architecture": {
+			Description: "The node's CPU architecture.",
+			TypeSpec:    types.LocalType("Architecture", "remote"),
+		},
+		"audiLogPath":                           props.String("The path to store the audit log file."),
+		"caCertificatePath":                     props.String("The path to the root certificate authority certificate."),
+		"caPrivateKeyPath":                      props.String("The path to the root certificate authority private key."),
+		"clusterCIDR":                           props.String("The cluster CIDR."),
+		"clusterName":                           props.String("The cluster name."),
+		"connection":                            props.Connection(commandSpec),
+		"encryptionConfigYaml":                  props.String("The v1/EncryptionConfig yaml."),
+		"kubeApiServerCertificatePath":          props.String("The path to the kube-apiserver certificate."),
+		"kubeApiServerInstallDirectory":         props.String("The directory to store the kube-apiserver binary."),
+		"kubeApiServerPrivateKeyPath":           props.String("The path to the kube-apiserver private key."),
+		"kubeControllerManagerInstallDirectory": props.String("The directory to store the kube-controller-manager binary."),
+		"kubeControllerManagerKubeconfigPath":   props.String("The path to the kube-controller-manager kubeconfig file."),
+		"kubectlInstallDirectory":               props.String("The path to store the kubectl binary."),
+		"kubernetesVersion":                     props.String("The version of kubernetes to use."),
+		"kubeSchedulerConfigYaml":               props.String("The kube-scheduler config yaml."),
+		"kubeSchedulerInstallDirectory":         props.String("The directory to store the kube-scheduler binary."),
+		"kubeSchedulerKubeconfigPath":           props.String("The path to the kube-scheduler kubeconfig file."),
+		"nodeName":                              props.String("The name of the node."),
+		"serviceAccountsCertificatePath":        props.String("The path to the service accounts certificate."),
+		"serviceAccountsPrivateKeyPath":         props.String("The path to the service accounts private key."),
+		"serviceClusterIpRange":                 props.String("The IP range to use for cluster services."),
 	}
 
-	requiredInputs := []string{"connection"}
+	requiredInputs := []string{
+		"apiServerCount",
+		"architecture",
+		"connection",
+		"caCertificatePath",
+		"caPrivateKeyPath",
+		"encryptionConfigYaml",
+		"kubeApiServerCertificatePath",
+		"kubeApiServerPrivateKeyPath",
+		"kubeControllerManagerKubeconfigPath",
+		"kubeSchedulerConfigYaml",
+		"kubeSchedulerKubeconfigPath",
+		"serviceAccountsCertificatePath",
+		"serviceAccountsPrivateKeyPath",
+	}
 
 	outputs := map[string]schema.PropertySpec{
+		"encryptionConfigFile": {
+			Description: "The remote encryption config file.",
+			TypeSpec:    types.LocalResource("File", "remote"),
+		},
 		"kubeApiServerInstall": {
 			Description: "The kube-apiserver install.",
 			TypeSpec:    types.LocalResource("KubeApiServerInstall", "remote"),
