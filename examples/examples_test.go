d8173dba32a7a989129421371d306ad6f4515eed
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-26 19:24:10 -0500

    Refactoring test container setup

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -7,11 +7,8 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go/network"
-	"golang.org/x/exp/maps"
 )
 
 func getBaseOptions(t *testing.T) integration.ProgramTestOptions {
@@ -63,49 +60,8 @@ func (n node) ReadFile(t *testing.T, file string) string {
 	return result
 }
 
-func newNode(t *testing.T, opts ...SshServerOption) node {
-	ctx := context.Background()
-	server, err := StartSshServer(ctx, opts...)
-	require.NoError(t, err, "failed to start ssh server")
-
-	t.Cleanup(func() {
-		err := StopSshServer(ctx, server)
-		require.NoError(t, err)
-	})
-
-	port, err := server.Port(ctx)
-	require.NoError(t, err, "failed to get node port")
-
-	ip, err := server.Ip(ctx)
-	require.NoError(t, err, "failed to retrieve node IP")
-
-	return node{Server: server, Port: port, Ip: ip}
-}
-
-func newCluster(t *testing.T, config map[string][]SshServerOption) map[string]node {
-	ctx := context.Background()
-	network, err := network.New(ctx, network.WithCheckDuplicate())
-	require.NoError(t, err, "failed to create network")
-
-	t.Cleanup(func() {
-		require.NoError(t, network.Remove(ctx))
-	})
-
-	nodes := map[string]node{}
-	for _, key := range maps.Keys(config) {
-		opts := append(config[key], WithNetwork(network.Name))
-		nodes[key] = newNode(t, opts...)
-	}
-
-	return nodes
-}
-
 func expectKey[T comparable, V any](t *testing.T, m map[T]V, key T, value V) {
 	actual, ok := m[key]
 	assert.Truef(t, ok, "Key `%s` was not set", key)
 	assert.Equal(t, value, actual)
-}
-
-func expectOutput(t *testing.T, res apitype.ResourceV3, key string, value interface{}) {
-	expectKey(t, res.Outputs, key, value)
 }
--- examples/resource_test.go
+++ examples/resource_test.go
@@ -1,6 +1,7 @@
 package examples
 
 import (
+	"context"
 	"fmt"
 	"path"
 	"testing"
@@ -9,10 +10,17 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go/network"
 	"golang.org/x/exp/maps"
 )
 
 type ResourceValidator func(t *testing.T, res apitype.ResourceV3)
+
+type ResourceTestSetup struct {
+	Config map[string]string
+	Setup  func(t *testing.T)
+}
 
 type ResourceContext struct {
 	tokens map[validatorKey]ResourceValidator
@@ -30,24 +38,12 @@ func Validate(ctx *ResourceContext, typ tokens.Type, name string, validator Reso
 func ResourceTest(t *testing.T, project string, baseOptions integration.ProgramTestOptions, validation func(ctx *ResourceContext)) {
 	skipIfShort(t)
 
-	const (
-		username = "root"
-		password = "root"
-	)
-
-	node := newNode(t,
-		WithSshUsername(username),
-		WithSshPassword(password),
-	)
+	if _, ok := baseOptions.Config["test:container"]; !ok {
+		baseOptions = baseOptions.With(SingleContainerSetup(t))
+	}
 
 	test := baseOptions.With(integration.ProgramTestOptions{
 		Dir: path.Join(getCwd(t), project),
-		Config: map[string]string{
-			"host":     "localhost",
-			"port":     node.Port,
-			"user":     username,
-			"password": password,
-		},
 		ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 			ctx := ResourceContext{tokens: map[validatorKey]ResourceValidator{}}
 			validation(&ctx)
@@ -74,4 +70,105 @@ func ResourceTest(t *testing.T, project string, baseOptions integration.ProgramT
 	})
 
 	integration.ProgramTest(t, &test)
+}
+
+func SingleContainerSetup(t *testing.T) integration.ProgramTestOptions {
+	const (
+		username = "root"
+		password = "root"
+	)
+
+	node := newNode(t,
+		WithSshUsername(username),
+		WithSshPassword(password),
+	)
+
+	return integration.ProgramTestOptions{
+		Config: map[string]string{
+			"test:container": "single",
+			"host":           "localhost",
+			"port":           node.Port,
+			"user":           username,
+			"password":       password,
+		},
+	}
+}
+
+func MultiContainerSetup(t *testing.T) integration.ProgramTestOptions {
+	const (
+		username = "root"
+		password = "root"
+	)
+
+	opts := []SshServerOption{
+		WithSshUsername(username),
+		WithSshPassword(password),
+	}
+	nodes := newCluster(t, map[string][]SshServerOption{
+		"node1": opts,
+		"node2": opts,
+		"node3": opts,
+	})
+
+	return integration.ProgramTestOptions{
+		Config: map[string]string{
+			"test:container": "multi",
+			"node1-host":     "localhost",
+			"node1-ip":       nodes["node1"].Ip,
+			"node1-port":     nodes["node1"].Port,
+			"node1-user":     username,
+			"node1-password": password,
+			"node2-host":     "localhost",
+			"node2-ip":       nodes["node2"].Ip,
+			"node2-port":     nodes["node2"].Port,
+			"node2-user":     username,
+			"node2-password": password,
+			"node3-host":     "localhost",
+			"node3-ip":       nodes["node3"].Ip,
+			"node3-port":     nodes["node3"].Port,
+			"node3-user":     username,
+			"node3-password": password,
+		},
+	}
+}
+
+func expectOutput(t *testing.T, res apitype.ResourceV3, key string, value interface{}) {
+	expectKey(t, res.Outputs, key, value)
+}
+
+func newNode(t *testing.T, opts ...SshServerOption) node {
+	ctx := context.Background()
+	server, err := StartSshServer(ctx, opts...)
+	require.NoError(t, err, "failed to start ssh server")
+
+	t.Cleanup(func() {
+		err := StopSshServer(ctx, server)
+		require.NoError(t, err)
+	})
+
+	port, err := server.Port(ctx)
+	require.NoError(t, err, "failed to get node port")
+
+	ip, err := server.Ip(ctx)
+	require.NoError(t, err, "failed to retrieve node IP")
+
+	return node{Server: server, Port: port, Ip: ip}
+}
+
+func newCluster(t *testing.T, config map[string][]SshServerOption) map[string]node {
+	ctx := context.Background()
+	network, err := network.New(ctx, network.WithCheckDuplicate())
+	require.NoError(t, err, "failed to create network")
+
+	t.Cleanup(func() {
+		require.NoError(t, network.Remove(ctx))
+	})
+
+	nodes := map[string]node{}
+	for _, key := range maps.Keys(config) {
+		opts := append(config[key], WithNetwork(network.Name))
+		nodes[key] = newNode(t, opts...)
+	}
+
+	return nodes
 }
