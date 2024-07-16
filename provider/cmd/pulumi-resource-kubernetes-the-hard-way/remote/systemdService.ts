f10a24853f9995a4a9edcd79205f372af895d7ac
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-15 23:56:57 -0500

    Formatting

--- provider/cmd/pulumi-resource-kubernetes-the-hard-way/remote/systemdService.ts
+++ provider/cmd/pulumi-resource-kubernetes-the-hard-way/remote/systemdService.ts
@@ -61,5 +61,5 @@ function sprint(value: Record<string, unknown>): string {
 }
 
 function capitalize(s: string): string {
-    return s[0].toUpperCase() + s.slice(1);
+  return s[0].toUpperCase() + s.slice(1);
 }
